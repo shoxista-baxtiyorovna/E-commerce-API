d74e55245fa8ecee29054ec87fe583b2566bee96
unknown <shoxista>
2025-04-02 17:29:42 +0300

    in process

---
+++ profiles/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    is_premium = models.BooleanField(default=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
