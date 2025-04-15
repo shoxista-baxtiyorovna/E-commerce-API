f17ad96925e387245aecaf29d3c147fb2a60d752
unknown <shoxista>
2025-04-15 17:43:06 +0300

    in process

--- products/serializers.py
+++ products/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from .models import Product
 
 
+class ProductCategorySerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(read_only=True)
+    description = serializers.CharField(read_only=True)
+
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
@@ -17,8 +23,10 @@ class ProductSerializer(serializers.ModelSerializer):
             'updated_at'
         ]
 
-        # def to_representation(self, instance):
-            # data = super().to_representation(instance)
+        def to_representation(self, instance):
+            data = super().to_representation(instance)
+            data['category'] = ProductCategorySerializer(instance.category).data
+            return data
 
 
         
